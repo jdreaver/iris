a77a1eea74c65451e335a5d3612fb22ae1d84d6e
John David Reaver <johndreaver@gmail.com>
2015-12-20 17:38:08 -0800

    Have clip example viewports dynamically resize

--- examples/Clip.hs
+++ examples/Clip.hs
@@ -2,8 +2,10 @@
 
 module Main where
 
+import           Control.Lens
 import qualified Graphics.Rendering.OpenGL as GL
 
+import           Iris.Backends
 import qualified Iris.Backends.GLFW as W
 import           Iris.Camera
 import           Iris.Reactive
@@ -16,17 +18,32 @@ main =
   do win <- W.makeWindow "Line Plot" (640, 480)
      canvas <- W.initGLFW win
 
-     cube <- makeCube
+     network <- compile $ makeNetwork canvas
+     actuate network
 
-     let clip1 = clipperNode (Viewport (GL.Position 0 0) (GL.Size 320 480)) [cube]
-         clip2 = clipperNode (Viewport (GL.Position 320 0) (GL.Size 320 480)) [cube]
+     W.mainLoop canvas
+
+
+makeNetwork :: (Canvas a) => a -> MomentIO ()
+makeNetwork can =
+  do (viewB1, viewB2) <- clipBehaviors can
+     cube <- liftIO makeCube
+     let clip1 = flip clipperNode [cube] <$> viewB1
+         clip2 = flip clipperNode [cube] <$> viewB2
          cam  = arcBallCamera { arcBallWidth     = 6
                               , arcBallAzimuth   = 30 * pi / 180
                               , arcBallElevation = 30 * pi / 180
                               }
-         scene = groupNode [clip1, clip2]
+         scene = groupNode <$> sequenceA [clip1, clip2]
+     makeScene can scene (Just cam)
 
-     network <- compile $ makeScene canvas (pure scene) (Just cam)
-     actuate network
 
-     W.mainLoop canvas
+-- | Create a Behavior for the viewport sizes. Each viewport takes up half of
+-- the screen.
+clipBehaviors :: (Canvas a) => a -> MomentIO (Behavior Viewport, Behavior Viewport)
+clipBehaviors c =
+  do events <- makeEvents c
+     let buffSizeB = events ^. framebufferSizeObservable ^. behavior
+         f1 (GL.Size w h) = Viewport (GL.Position 0 0) (GL.Size (w `quot` 2) h)
+         f2 (GL.Size w h) = Viewport (GL.Position (w `quot` 2) 0) (GL.Size (w `quot` 2) h)
+     return (f1 <$> buffSizeB, f2 <$> buffSizeB)
